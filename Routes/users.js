78a73b26a803dd2f9854d5b010612e8d144dcedc
emilio <emili115kariuki@gmail.com>
2022-06-30 12:20:25 +0300

    Finished the application

--- Routes/users.js
+++ Routes/users.js
@@ -1,9 +1,36 @@
 import express from 'express';
+import {v4 as uuidv4 }from 'uuid';
 
 const router = express.Router();
 
+let users = [];
+
+//used to get the data from the json
 router.get('/', (req,res) =>{
-    res.send("This is the main route") ;
+  res.send(users);
+});
+//used tpo send the data to the array
+
+router.post('/', (req,res) =>{
+  const user = req.body;
+  users.push({...user, id: uuidv4()});
+  res.send(users);
+});
+
+//used to update the data in the array
+
+router.delete('/:id', (req,res) =>{
+  const {id} = req.params;
+  users = users.filter((user) => user.id != id);
+  res.send(`user with the id ${id} has been deleted`);
+})
+
+router.get('/:id', (req,res) => {
+  const {id} = req.params;
+
+  users = users.find((user) => user.id == id);
+
+  res.send(users);
 })
 
 export default router;
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import expressRouter from './Routes/users.js';
+import usersRouter from './Routes/users.js';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use('/users', expressRouter);
+app.use('/users', usersRouter);
+
 
 app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
 
-    console.log("Server is running on port 3000 http://localhost:3000");
-})
+});
+
+
